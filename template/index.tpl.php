293a98b72367bc41a5b1c4ee3a0600766e7f502e
daxingplay <daxingplay@gmail.com>
2012-12-25 17:43:43 +0800

    add google analytics code

--- template/index.tpl.php
+++ template/index.tpl.php
@@ -177,5 +177,18 @@ if(!defined('IN_APP')){
 <script type="text/javascript" src="<?php echo $_G['assets']; ?>common/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo $_G['assets']; ?>bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo $_G['assets']; ?>index/1.0/index.js"></script>
+<script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-37265516-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
 </body>
 </html>
